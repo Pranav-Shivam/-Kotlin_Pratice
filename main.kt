e3695b8d1ab9714b0a67a5d5a2076b7cd7971183
Pranav Singh <>
2022-08-05 05:41:33 +0000

    first commit

--- main.kt
+++ main.kt
@@ -1,7 +1,30 @@
 import java.util.Scanner
 fun main() {
   var sc=Scanner(System.`in`)
-  println("Enter a no")
-  var z:Int=sc.nextInt()
-  println("$z")
+  println("\n")
+  println("Enter a,b and c so that we can find out the nature of the root ")
+  println("Where Equation is ax^2+bx+c =0 ")
+  println("Enter a ")
+  var a:Int=sc.nextInt()
+  println("Enter b ")
+  var b:Int=sc.nextInt()
+  println("Enter c ")
+  var c:Int=sc.nextInt()
+  val D=b*b-4*a*c
+  if(D!=0)
+  {
+    if(D>0)
+    {
+      println("Root are real and distinct.")
+    }
+    else
+    {
+      println("Root are not real means complex and distinct.")
+    }
+  }
+  else
+  {
+      println("Root are real and equal")
+  }
+
 }
